92f7968d6c1c9d276f1330b9099482aca16fc9ec
DarrenTZF <231150Z@mymail.nyp.edu.sg>
2025-09-25 14:36:40 +0800

    made the welcome message look better

--- reactPortfolio/src/pages/home.tsx
+++ reactPortfolio/src/pages/home.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "../index.css";
-import "./home.css";
 
 const Home = () => {
   return (
     <div className="h-[calc(100vh-4rem)] flex justify-center items-center">
       {/* welcome message */}
-      <div className="bg-gray-800/30 w-11/12 md:w-3/4 lg:w-2/3 rounded-md p-10 flex justify-center items-center">
+      <div className="bg-gray-800/30 w-11/12 md:w-3/4 lg:w-2/3 rounded-md p-10 flex justify-center items-center border-sky-500 border-2 shadow-[0_0_20px_2px_rgba(56,189,248,0.7)] animate-glow">
         <p>
           {"Welcome to my portfolio!".split("").map((letter, i) => (
             <span
